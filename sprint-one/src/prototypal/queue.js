9b104fa0afd55c2de2948a36f853f3bb0fd0ed91
liebeskind <daniel.liebeskind@gmail.com>
2013-09-25 15:50:33 -0700

    completed prototypal implementation

--- sprint-one/src/prototypal/queue.js
+++ sprint-one/src/prototypal/queue.js
@@ -1,6 +1,32 @@
-var makeQueue = function() {
-  // Hey! Copy your code from src/functional-shared/queue.js and paste it here
+var makeQueue = function(){
+  var instance = Object.create(queueMethods);
+
+  instance.storage = {};  
+  instance.mySize = 0;
+  instance.front = 0;
+
+  return instance;
 };
 
-var queueMethods = {};
+queueMethods = {};
+
+queueMethods.size = function(){
+  return this.mySize;
+}
+
+queueMethods.enqueue = function(value){
+    this.storage[this.mySize + this.front] = value;
+    this.mySize++;
+};
+
+queueMethods.dequeue = function(){
+  var fval = this.storage[this.front];
+  delete this.storage[this.front];
+  if(this.mySize){
+    this.front++;
+    this.mySize--;
+  }
+  return fval;
+};
+
 
