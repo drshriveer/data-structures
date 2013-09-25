5c7260d850fea50e284d7467a5bb037bbef897c1
liebeskind <daniel.liebeskind@gmail.com>
2013-09-25 15:35:51 -0700

    completed shared function variation

--- sprint-one/src/functional-shared/stack.js
+++ sprint-one/src/functional-shared/stack.js
@@ -1,6 +1,28 @@
-var makeStack = function() {
-  // Hey! Copy your code from src/functional/stack.js and paste it here
+var makeStack = function(){
+  var instance = {};
+  instance.storage = {};
+  instance.mySize = 0; // Hint: set an initial value here
+
+  _.extend(instance, stackMethods); 
+
+  return instance;
 };
 
 var stackMethods = {};
+/////////////////////////////////////////
+stackMethods.push = function(value){
+    
+    this.mySize++;
+    this.storage[this.mySize] = value;
+};
 
+stackMethods.pop = function(){
+  var val = this.storage[this.mySize];
+  delete this.storage[this.mySize];
+  this.mySize && this.mySize--;
+  return val;  
+};
+
+stackMethods.size = function(){
+  return this.mySize;
+};
