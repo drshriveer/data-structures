7bd5f0571e8bda429b29a9b774752d1a5af0925f
liebeskind <daniel.liebeskind@gmail.com>
2013-09-25 16:25:30 -0700

    completed pseudoclassical implementation

--- sprint-one/src/pseudoclassical/queue.js
+++ sprint-one/src/pseudoclassical/queue.js
@@ -1,4 +1,27 @@
-var Queue = function() {
-  // Hey! Copy your code from src/prototypal/queue.js and paste it here
+var Queue = function(){
+
+  this.storage = {};  
+  this.mySize = 0;
+  this.front = 0;
 };
+
+Queue.prototype.size = function(){
+  return this.mySize;
+}
+
+Queue.prototype.enqueue = function(value){
+    this.storage[this.mySize + this.front] = value;
+    this.mySize++;
+};
+
+Queue.prototype.dequeue = function(){
+  var fval = this.storage[this.front];
+  delete this.storage[this.front];
+  if(this.mySize){
+    this.front++;
+    this.mySize--;
+  }
+  return fval;
+};
+
 
--- sprint-one/src/pseudoclassical/stack.js
+++ sprint-one/src/pseudoclassical/stack.js
@@ -1,4 +1,23 @@
-var Stack = function() {
-  // Hey! Copy your code from src/prototypal/stack.js and paste it here
+var Stack = function(){
+
+  this.storage = {};
+  this.mySize = 0; 
+
 };
 
+Stack.prototype.push = function(value){
+    
+    this.mySize++;
+    this.storage[this.mySize] = value;
+};
+
+Stack.prototype.pop = function(){
+  var val = this.storage[this.mySize];
+  delete this.storage[this.mySize];
+  this.mySize && this.mySize--;
+  return val;  
+};
+
+Stack.prototype.size = function(){
+  return this.mySize;
+};
