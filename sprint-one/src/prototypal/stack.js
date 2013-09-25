e2a3ba07788cb50c92bea6fe8dd2be2804856ca5
liebeskind <daniel.liebeskind@gmail.com>
2013-09-25 16:13:36 -0700

    stack.js implemented in prototypal fashion

--- sprint-one/src/prototypal/stack.js
+++ sprint-one/src/prototypal/stack.js
@@ -1,6 +1,26 @@
-var makeStack = function() {
-  // Hey! Copy your code from src/functional-shared/stack.js and paste it here
+var makeStack = function(){
+  var instance = Object.create(stackMethods);
+  instance.storage = {};
+  instance.mySize = 0; // Hint: set an initial value here
+
+  return instance;
 };
 
-var stackMethods = {};
+stackMethods = {};
+/////////////////////////////////////////
+stackMethods.push = function(value){
+    
+    this.mySize++;
+    this.storage[this.mySize] = value;
+};
 
+stackMethods.pop = function(){
+  var val = this.storage[this.mySize];
+  delete this.storage[this.mySize];
+  this.mySize && this.mySize--;
+  return val;  
+};
+
+stackMethods.size = function(){
+  return this.mySize;
+};
