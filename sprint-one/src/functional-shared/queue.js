87738c1216dccbebcce16e5bea45210d59dcc5d0
liebeskind <daniel.liebeskind@gmail.com>
2013-09-25 15:41:45 -0700

    makeQueue is fully functional

--- sprint-one/src/functional-shared/queue.js
+++ sprint-one/src/functional-shared/queue.js
@@ -1,6 +1,34 @@
 var makeQueue = function(){
-  // Hey! Copy your code from src/functional/queue.js and paste it here
+  var instance = {};
+
+  instance.storage = {};  
+  instance.mySize = 0;
+  instance.front = 0;
+
+  _.extend(instance,queueMethods);
+
+  return instance;
 };
 
 var queueMethods = {};
+
+queueMethods.size = function(){
+  return this.mySize;
+}
+
+queueMethods.enqueue = function(value){
+    this.storage[this.mySize + this.front] = value;
+    this.mySize++;
+};
+
+queueMethods.dequeue = function(){
+  var fval = this.storage[this.front];
+  delete this.storage[this.front];
+  if(this.mySize){
+    this.front++;
+    this.mySize--;
+  }
+  return fval;
+};
+
 
