43375417045fa7fa311ccf1bcaa0bda86f7263a9
Gavin Shriver <gavin.shriver@gmail.com>
2013-09-26 12:55:32 -0700

    made pass all tests

--- sprint-two/src/tree.js
+++ sprint-two/src/tree.js
@@ -1,15 +1,29 @@
-var makeTree = function(){
+var makeTree = function(value){
   var newTree = {};
-  newTree.value = undefined;
-  newTree.children = undefined;
+  newTree.value = value;
+  newTree.children = []; //has property length
+
+  _.extend(newTree, treeMethods);
 
   return newTree;
 };
 
 var treeMethods = {};
 
-treeMethods.addChild = function(){
+treeMethods.addChild = function(value){
+  this.children.push(makeTree(value));
 };
 
-treeMethods.contains = function(){
+treeMethods.contains = function(value){
+  var results = [];
+
+  if(this.value === value){
+    results.push(true);
+  }
+  _(this.children).each(function(childNode){
+    results = results.concat(childNode.contains(value),results);
+  });
+
+  return _.some(results);
+
 };
