e06ad437feccc904b0f42e0a2eaaef22ed2fd91e
Gavin Shriver <gavin.shriver@gmail.com>
2013-09-26 14:18:14 -0700

    added test for 'add'

--- sprint-two/spec/setSpec.js
+++ sprint-two/spec/setSpec.js
@@ -10,4 +10,17 @@ describe("set", function() {
     expect(set.contains).toEqual(jasmine.any(Function));
     expect(set.remove).toEqual(jasmine.any(Function));
   });
+
+    it("after add should add specific elemets", function() {
+    set.add('a');
+    set.add('b');
+    set.add('c');
+    set.add('d');
+    set.add('e');
+    expect(set._storage['a'].toEqual('a'));
+    expect(set._storage['b'].toEqual('b'));
+    expect(set._storage['c'].toEqual('c'));
+    expect(set._storage['d'].toEqual('d'));
+  });
+
 });
