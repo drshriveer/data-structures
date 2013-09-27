e2bc8b524747dcb780735899348bb5638df2cecc
Gavin Shriver <gavin.shriver@gmail.com>
2013-09-26 21:07:22 -0700

    broken changes from an 'object based' solution to an array based solution

--- sprint-two/src/hashTable.js
+++ sprint-two/src/hashTable.js
@@ -13,19 +13,37 @@ var HashTable = function(){
 };
 
 HashTable.prototype.insert = function(k, v){
+
   var i = getIndexBelowMaxForKey(k, this._limit);
   console.log(i);
-  this._storage.set(i, v);
+
+  // if(this._storage.get(i) === undefined){
+  //   this._storage.set(i, [[k,v]]);
+  // }else if ( _(this._storage.get(i)).reduce(function(memo, keyPair){
+  //   return memo || (keyPair[0] === k);
+  // },false) ){
+  //   this._storage.get(i).push([k,v]);
+  // }
+
+  console.log(i);
 };
 
 HashTable.prototype.retrieve = function(k){
   var i = getIndexBelowMaxForKey(k, this._limit);
-  return this._storage.get(i);
+  for (var j = 0; j < this._storage(i).length; j++) {
+    if( this._storage.get(i)[j][0] === k){
+      return this._storage.get(i)[j][1];
+    }
+  }
+  return;
 };
 
-HashTable.prototype.remove = function(){
+HashTable.prototype.remove = function(k){
+  var i = getIndexBelowMaxForKey(k, this._limit);
+  delete this._storage.get(i)[k];
 };
 
 // NOTE: For this code to work, you will NEED the code from hashTableHelpers.js
 // Start by loading those files up and playing with the functions it provides.
 // You don't need to understand how they work, only their interface is important to you
+
