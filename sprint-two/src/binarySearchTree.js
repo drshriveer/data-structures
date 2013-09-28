c2c07cb977a2f92acfa07f859eda54d5361d1925
Gavin Shriver <gavin.shriver@gmail.com>
2013-09-27 20:03:56 -0700

    one implementation of insert working only for one insert

--- sprint-two/src/binarySearchTree.js
+++ sprint-two/src/binarySearchTree.js
@@ -1,23 +1,36 @@
-var makeBinarySearchTree = function(){
+var makeBinarySearchTree = function(value){
   var bTree = Object.create(bTreePrototype);
 
+  bTree.value = value;
   bTree.left = null;
   bTree.right = null;
-
 
   return bTree;
 };
 
 var bTreePrototype = {};
 
-  bTreePrototype.insert = function(){
-  };
+bTreePrototype.insert = function(value){
 
-  bTreePrototype.contains = function(){
-  };
+  if(value < this.value && this.left === null){
+    this.left = makeBinarySearchTree(value);
+  }else if(value < this.value && this.left !== null){
+    this.left.insert(value);
+  }else if( value > this.value && this.right === null){
+    this.right = makeBinarySearchTree(value);
+  }else if( value > this.value && this.right !== null){
+    this.left.insert(value);
+  }else{
+    //same value do nothing bitch!
+  }
 
-  bTreePrototype.depthFirstLog = function(){  
-  };
+};
+
+bTreePrototype.contains = function(){
+};
+
+bTreePrototype.depthFirstLog = function(){  
+};
 
 
 
