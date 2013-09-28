c1ca1ea245b9a3060795a787ee58faaee1a8f08b
Gavin Shriver <gavin.shriver@gmail.com>
2013-09-27 19:47:30 -0700

    added test for insert

--- sprint-two/spec/binarySearchTreeSpec.js
+++ sprint-two/spec/binarySearchTreeSpec.js
@@ -11,4 +11,24 @@ describe("binarySearchTree", function() {
     expect(binarySearchTree.depthFirstLog).toEqual(jasmine.any(Function));
   });
   // add more tests here to test the functionality of binarySearchTree
+
+  it("should insert multiple items", function(){
+    binarySearchTree.insert(3);
+    binarySearchTree.insert(1);
+    binarySearchTree.insert(6);
+    binarySearchTree.insert(10);
+
+
+    expect(binarySearchTree.value).toEqual(3);
+    expect(binarySearchTree.left.value).toEqual(1);
+    expect(binarySearchTree.right.value).toEqual(6);
+    expect(binarySearchTree.right.right.value).toEqual(10);
+
+  });
+
+  it("should use .contains to find inserted items", function(){
+
+  });
+
+
 });
