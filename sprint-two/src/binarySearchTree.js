79f7e2312d815e77f4e968f59246b6b31b4b7603
Gavin Shriver <gavin.shriver@gmail.com>
2013-09-27 19:47:52 -0700

    added functions and variables

--- sprint-two/src/binarySearchTree.js
+++ sprint-two/src/binarySearchTree.js
@@ -1,2 +1,23 @@
 var makeBinarySearchTree = function(){
+  var bTree = Object.create(bTreePrototype);
+
+  bTree.left = null;
+  bTree.right = null;
+
+
+  return bTree;
 };
+
+var bTreePrototype = {};
+
+  bTreePrototype.insert = function(){
+  };
+
+  bTreePrototype.contains = function(){
+  };
+
+  bTreePrototype.depthFirstLog = function(){  
+  };
+
+
+
